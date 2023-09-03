d8aa3e8bd48c631e4f8f81c10492edefdc5c594f
Yallison Gabriel Reis <gabrielyallison2012@hotmail.com>
2023-09-03 04:28:28 -0400

    Crate handle events in rows from table

--- src/js/index.js
+++ src/js/index.js
@@ -20,23 +20,25 @@ class RootApp extends HTMLDivElement {
   proxy() {
 
     let tableItems = setState('tableItems', [
-        `<tr data-key="1" class="bg-secondary text-decoration-line-through">
-          <td><input type="checkbox" name="todo-check" checked/></td>
+        `<tr data-key="1" class="bg-secondary text-decoration-line-through" is="list-table-tr">
+          <td><input type="checkbox" name="todo-check" checked class="on-check-done"/></td>
           <td>Ir ao mercado</td>
-          <td class="actions"><button type="button" class="btn btn-outline-light btn-sm">Refazer</button></td>
+          <td class="actions"><button type="button" class="btn btn-outline-light btn-sm on-check-undone">Refazer</button></td>
         </tr>`,
-        `<tr data-key="2">
-          <td><input type="checkbox" name="todo-check"/></td>
-          <td>Ir a academia</td>
-          <td class="actions"><button type="button" class="btn btn-sm btn-danger">Excluir</button></td>
+        `<tr data-key="2" is="list-table-tr">
+          <td><input type="checkbox" name="todo-check" class="on-check-done"/></td>
+          <td class="on-dbl-click-edit">Ir a academia</td>
+          <td class="actions">
+            <button type="button" class="btn btn-secondary btn-sm on-edit">Editar</button>
+            <button type="button" class="btn btn-sm btn-danger on-delete">Excluir</button>
+          </td>
         </tr>
         `,
-        `<tr>
+        `<tr is="list-table-tr">
             <td></td>
-            <td class="p-0"><input type="text" class="form-control w-100 b-none" style="height: 48px !important; box-shadow: none; border: none;"/></td>
+            <td class="p-0"><input type="text" class="form-control w-100 b-none on-input" style="height: 48px !important; box-shadow: none; border: none;"/></td>
             <td class="actions">
-              <button type="button" class="btn btn-sm btn-primary">Adicionar</button>
-              <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+              <button type="button" class="btn btn-sm btn-primary on-add">Adicionar</button>
             </td>
           </tr>`,
       ]);
--- src/js/table.js
+++ src/js/table.js
@@ -39,4 +39,59 @@ class AppTable extends HTMLTableElement {
 
 }
 
+class AppTableRow extends HTMLTableRowElement {
+  constructor() {
+    super();
+
+    this.onEvents();
+
+  }
+
+  get dataKey(){
+    return this.getAttribute('data-key');
+  }
+  
+  onEvents() {
+    this.find('input')?.addEventListener('input', (e) => {
+      const value = e.target.value;
+
+      console.log('value', value);
+    });
+
+    this.find('.on-check-done')?.addEventListener('change', (e) => {
+      const key = this.dataKey;
+      console.log('on-check-done', key);
+    });
+
+    this.find('.on-check-undone')?.addEventListener('click', (e) => {
+      const key = this.dataKey;
+      console.log('on-check-undone', key);
+    });
+
+    this.find('.on-add')?.addEventListener('click', (e) => {
+      const key = this.dataKey;
+      console.log('on-add', key);
+    });
+
+    this.find('.on-edit')?.addEventListener('click', (e) => {
+      const key = this.dataKey;
+      console.log('on-add', key);
+    });
+
+    this.find('.on-dbl-click-edit')?.addEventListener('click', (e) => {
+      const key = this.dataKey;
+      console.log('on-add', key);
+    });
+
+    this.find('.on-delete')?.addEventListener('click', (e) => {
+      const key = this.dataKey;
+      console.log('on-delete', key);
+    });
+  }
+
+
+}
+
 customElements.define("list-table", AppTable, {extends: 'table'});
+
+customElements.define("list-table-tr", AppTableRow, {extends: 'tr'});
